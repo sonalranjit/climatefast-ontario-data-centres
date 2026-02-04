cfbfe3cfa29e12e508a58264717faee9b49fc73e
Andre053 <andre.61122@proton.me>
2026-02-04 10:50:09 -0500

    Resolved nested repository issue

---
+++ data-centre-directory/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import '../globals.css';
+
+export default async function Home() {
+
+    return (
+        <div className="text-center">
+            <h1 className="text-4xl mt-5">
+                About page
+            </h1>
+        </div>
+        
+    );
+};
---
+++ data-centre-directory/src/app/map/page.tsx
@@ -0,0 +1,13 @@
+import '../globals.css';
+
+export default async function Home() {
+
+    return (
+        <div className="text-center">
+            <h1 className="text-4xl mt-5">
+                Map page
+            </h1>
+        </div>
+        
+    );
+};
